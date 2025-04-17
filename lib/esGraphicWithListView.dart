75e6db92f92984d2c83f0fb17b1c482eb6d6c774
noemi <dingeonoemi@gmail.com>
2025-04-17 12:33:32 +0200

    17 aprile

--- lib/esGraphicWithListView.dart
+++ lib/esGraphicWithListView.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 /*punto di partenza dell'applicazione*/
 void main() => runApp(FutureBuilderAppExample());
 
-
 /*
 * configura il MaterialApp e imposta TextFieldWithFuture
 * */
@@ -39,25 +38,35 @@ class TextFieldWithFuture extends StatefulWidget {
 class _TextFieldWithFutureState extends State<TextFieldWithFuture> {
   late Future<int> _futureInt;
   late TextEditingController controller;
+  late TextInfoController textInfoController;
+  late TextInfoController listElementNumbersController;
   late List<int> randomNumbers = [];
 
   @override
-  void initState() { // inizializzazione risorse
+  void initState() {
+    // inizializzazione risorse
     _futureInt = _getInitialValue();
     controller = TextEditingController();
     controller.text = widget.initialText;
-    super.initState();
+    textInfoController = TextInfoController();
+    listElementNumbersController = TextInfoController();
 
-    _futureInt.then((value) { /* quando ha il future inizializza la lista di numeri */
-      setState(() {
-        randomNumbers = generateNumbers(value); /* ogni volta genera un numero casuale di numeri casuali */
-      });
-    });
+    _futureInt.then((v) =>
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          listElementNumbersController.changeNumber(v);
+          setState(() {
+            randomNumbers = generateNumbers(v);
+          });
+        })
+    );
+    super.initState();
   }
 
   @override
-  void dispose() { /*  serve per uso delle risorse (liberare) */
+  void dispose() {
+    /*  serve per uso delle risorse (liberare) */
     controller.dispose();
+    textInfoController.dispose();
     super.dispose();
   }
 
@@ -80,53 +89,124 @@ class _TextFieldWithFutureState extends State<TextFieldWithFuture> {
         builder: (context, snapshot) {
           return switch (snapshot.connectionState) {
             ConnectionState.none => const SizedBox.shrink(),
-            ConnectionState.waiting => Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextField(controller: controller),
-                  const SizedBox(width: 20),
-                  const CircularProgressIndicator(),
-                ],
-              ),
-            ),
-            ConnectionState.active => const CircularProgressIndicator(),
-            ConnectionState.done => Column(
+            ConnectionState.waiting => Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  '   Numero estratto: ${snapshot.data}   ',
-                  style: const TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    backgroundColor: Colors.orange
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: randomNumbers.length,
-                    itemBuilder: (context, index) => ListTile(
-                      title: Text(
-                        randomNumbers[index].toString(),
-                        style: const TextStyle(fontSize: 20),
-                      ),
-                      onTap: () {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text("Premuto: ${randomNumbers[index]}"),
-                            duration: const Duration(seconds: 2),
-                          ),
-                        );
-                      },
-                      tileColor: Colors.orangeAccent,
-                    ),
-                  ),
-                ),
+                Expanded(child: TextField(controller: controller)),
+                const SizedBox(width: 20),
+                const CircularProgressIndicator(),
               ],
             ),
+            ConnectionState.active => const CircularProgressIndicator(),
+            ConnectionState.done => _buildBody(snapshot.data!)
           };
         },
       ),
     );
+  }
+  
+  Widget _buildBody(int nNumbersToGenerate) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        _TextInfoWidget(
+          labelToDisplay: 'Numero elementi lista:',
+          controller: listElementNumbersController,
+          bkColor: Colors.orange,
+        ),
+        _TextInfoWidget(
+          bkColor: Colors.yellow,
+          labelToDisplay: 'Numero selezionato:',
+          controller: textInfoController,
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: randomNumbers.length,
+            itemBuilder:
+                (context, index) => ListTile(
+              title: Text(
+                randomNumbers[index].toString(),
+                style: const TextStyle(fontSize: 20),
+              ),
+              onTap: () {
+                textInfoController.changeNumber(randomNumbers[index]);
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      "Premuto: ${randomNumbers[index]}",
+                    ),
+                    duration: const Duration(seconds: 2),
+                  ),
+                );
+              },
+              tileColor: Colors.orangeAccent,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _TextInfoWidget extends StatefulWidget {
+  final TextInfoController? controller;
+  final String labelToDisplay;
+  final Color bkColor;
+  final int Function()? onNewSelectedNumber;
+
+  const _TextInfoWidget({
+    super.key,
+    this.controller,
+    required this.labelToDisplay,
+    this.onNewSelectedNumber,
+    this.bkColor = Colors.white
+  });
+
+  @override
+  State<_TextInfoWidget> createState() => _TextInfoWidgetState();
+}
+
+class _TextInfoWidgetState extends State<_TextInfoWidget> {
+
+  @override
+  void initState() {
+    widget.controller?.addListener(() {
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 20),
+      color: Colors.white,
+      child: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          color: widget.bkColor,
+          child: Text(
+            '${widget.labelToDisplay} ${widget.controller?.numberSelected ?? 'nessuno'}',
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TextInfoController extends ChangeNotifier {
+  int? _numberSelected;
+  int? get numberSelected => _numberSelected;
+
+  TextInfoController({int? numberSelected}) : _numberSelected = numberSelected;
+  changeNumber(int? numberSelected) {
+    _numberSelected = numberSelected;
+    notifyListeners();
   }
 }
